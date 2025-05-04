7037217de75b2f7492fac45f8ba821683148f069
symeeyee265 <kaisontoh@gmail.com>
2025-05-04 18:56:59 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/dbConnection.php';
-require_once __DIR__ . '/emailService.php';
+//require_once __DIR__ . '/emailService.php';
 require_once __DIR__ . '/oauth.php';
 require_once __DIR__ . '/eligibilityCheck.php';
 
